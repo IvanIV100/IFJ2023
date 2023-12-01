6c986d164f3b500b7a2caf228dceccacad167e73
mamelukosonix <146879952+mamelukosonix@users.noreply.github.com>
2023-12-01 17:12:20 +0100

    update

--- code_generator.c
+++ code_generator.c
@@ -6,12 +6,42 @@
  * @author Robert Zelníček <xzelni06@stud.fit.vutbr.cz>
  */
 
+<<<<<<< HEAD
+//TODO
+//ExpressionVariable generation (look)
+
+=======
+>>>>>>> d27f9842d0298b81609793bc523c57c6732cd08e
 #include "code_generator.h"
 
+<<<<<<< HEAD
+char *llvlVarForm = "%s@&_%s$%d_&";
+char *globVar1 = "GF@$%otempvar%$";
+char *globVar2 = "GF@$%otempvar2%$";
+=======
 //TODO
 //free temp_var
+>>>>>>> d27f9842d0298b81609793bc523c57c6732cd08e
 
 
+<<<<<<< HEAD
+    // Initialize frame and variables
+    initializeFrameAndVariables();
+
+    // Initialize stack parameter counter
+    printf("PUSHS int@0\n");
+
+    generateReadInt();
+    generateWrite();
+    //generatechr();
+    //generateDouble2Int();
+    //generateInt2Double();
+    //generateLength();
+    //generateord();
+    //generatereadDouble();
+    //generatereadSrting();
+    //generateSubstring();    
+=======
 // Function to check if an identifier corresponds to a built-in function
 bool is_builtin_function(const char* identifier) {
     for (int i = 0; built_in_functions[i] != NULL; ++i) {
@@ -20,12 +50,160 @@ bool is_builtin_function(const char* identifier) {
         }
     }
     return false;
+>>>>>>> d27f9842d0298b81609793bc523c57c6732cd08e
 }
 
 void start_code_generation(node_t* parse_tree) {
     generate_statement_list_code(parse_tree);
 }
 
+<<<<<<< HEAD
+void initializeFrame() {
+    printf("CREATEFRAME\n");
+}
+
+void defineAndMoveVariable(char *variable, char *value) {
+    defineVariable(variable);
+    move(variable, value);
+}
+
+void defineVariable(char *name) {
+    printf("DEFVAR %s\n", name);
+}
+
+void move(char *var, char *symb) {
+    printf("MOVE %s %s\n", var, symb);
+}
+
+void jump(char *lbl) {
+    printf("JUMP %s\n", lbl);
+}
+
+void address3Operator(char *op, char *res, char *s1, char *s2) {
+    if (strcmp(op, "CONCAT") == 0) {
+        printf("CONCAT %s %s %s\n", res, s1, s2);
+    } else if (strcmp(op, "OR") == 0) {
+        printf("OR %s %s %s\n", res, s1, s2);
+    } else if (strcmp(op, "AND") == 0) {
+        printf("AND %s %s %s\n", res, s1, s2);
+    } else if (strcmp(op, "NOT") == 0) {
+        printf("NOT %s %s\n", res, s1);
+    } else if (strcmp(op, "PLUS") == 0) {
+        printf("ADD %s %s %s\n", res, s1, s2);
+    } else if (strcmp(op, "MINUS") == 0) {
+        printf("SUB %s %s %s\n", res, s1, s2);
+    } else if (strcmp(op, "MUL") == 0) {
+        printf("MUL %s %s %s\n", res, s1, s2);
+    } else if (strcmp(op, "DIV") == 0) {
+        printf("DIV %s %s %s\n", res, s1, s2);
+    } else if (strcmp(op, "NEQ") == 0) {
+        printf("EQ %s %s %s\n", s1, s1, s2);
+        printf("NOT %s %s\n", res, s1);
+    }  else if (strcmp(op, "EQ") == 0) {
+        printf("EQ %s %s %s\n", res, s1, s2);
+    } else if (strcmp(op, "LT") == 0) {
+        printf("LT %s %s %s\n", res, s1, s2);
+    } else if (strcmp(op, "GT") == 0) {
+        printf("GT %s %s %s\n", res, s1, s2);
+    } else if (strcmp(op, "LTE") == 0) {
+        printf("LT %s %s %s\n", globVar1, s1, s2);
+        printf("EQ %s %s %s\n", globVar2, s1, s2);
+        printf("OR %s %s %s\n", res, globVar1, globVar2);
+    } else if (strcmp(op, "GTE") == 0) {
+        printf("GT %s %s %s\n", globVar1, s1, s2);
+        printf("EQ %s %s %s\n", globVar2, s1, s2);
+        printf("OR %s %s %s\n", res, globVar1, globVar2);
+    } else if (strcmp(op, "QQ") == 0) {
+        char *lbl1 = generateLabel();
+        char *lbl2 = generateLabel();
+        char *lbl3 = generateLabel();
+
+        printf("JUMPIFEQ %s %s %s\n", lbl1, s1, "nil@nil");
+        printf("JUMP %s\n", lbl2);
+        printf("LABEL %s\n", lbl1);
+        printf("MOVE %s %s\n", res, s2);
+        printf("JUMP %s\n", lbl3);
+        printf("LABEL %s\n", lbl2);
+        printf("MOVE %s %s\n", res, s1);
+        printf("LABEL %s\n", lbl3);
+
+        free(lbl1);
+        free(lbl2);
+        free(lbl3);
+    }
+}
+
+char *generateExpressionVariable() {
+    static int cnt = 0;
+    char mx[MAX_VAR];
+    sprintf(mx, "TF@$generated_on_stack_%d$", cnt);
+    printf("DEFVAR %s\n", mx);
+    char *res = malloc(sizeof(char) * (strlen(mx) + 1));
+    strcpy(res, mx);
+    cnt++;
+    return res;
+}
+
+char *getVarName(char *var) {
+    Symbol *Cur;
+    SymTable *tbl;
+    Searching(&tbl,var);
+
+    char mx[MAX_VAR];
+    char *frame;
+    int lvlNameAdd = -1;
+    if (runInfo->currentLVL == 0) {
+        frame = "GF";
+        //TOTO
+    } else if (runInfo->currentLVL->currentTab == runInfo->currentLVL) {
+        frame = "TF";
+    } else {
+        frame = "GF";
+        lvlNameAdd = runInfo->currentLVL;
+    }
+
+    if (lvlNameAdd == -1) {
+        sprintf(mx, "%s@%s", frame, var);
+    } else {
+        sprintf(mx, llvlVarForm, frame, var, lvlNameAdd);
+    }
+
+    char *res = malloc(sizeof(char) * (strlen(mx) + 1));
+    strcpy(res, mx);
+
+    return res;
+}
+
+char *convStr(const char *inputStr) {
+    size_t inputLength = strlen(inputStr);
+    char *resStr = (char *)malloc(sizeof(char) * inputLength * 5);
+    size_t i = 0, j = 0;
+    for (; i < inputLength; i++) {
+        char current = inputStr[i];
+
+        if (((int)current >= 0 && (int)current <= 32) || (int)current == 35 || (int)current == 92) {
+            sprintf(resStr + j, "\\%03d", (int)current);
+            j += 4;
+        } else {
+            resStr[j++] = current;
+        }
+    }
+    resStr[j] = '\0';
+    return resStr;
+}
+
+char *getLitName(Token liter) {
+    char mx[MAX_LIT];
+    switch (liter.Category) {
+        case NIL: {
+            sprintf(mx, "nil@nil");
+            break;
+        }
+        case T_KW_STRING: {
+            char *tmp = convStr(liter.value.stringVal);
+            sprintf(mx, "string@%s", tmp);
+            free(tmp);
+=======
 void generate_builtin_function_call(node_t* node) {
     if (strcmp(node->current->value.ID_name, "Int2Double") == 0) {
         generateInt2Double();
@@ -63,6 +241,7 @@ void generate_expression_code(node_t* node) {
         case T_STRING:
             // Generate code for a string literal
             printf("PUSHS string@%s\n", node->current->value.stringVal);
+>>>>>>> d27f9842d0298b81609793bc523c57c6732cd08e
             break;
 
         case T_MULTILINE_STRING:
@@ -89,6 +268,63 @@ void generate_expression_code(node_t* node) {
             // Handle other expression types if needed
             break;
     }
+<<<<<<< HEAD
+    char *res = malloc(sizeof(char) * (strlen(mx) + 1));
+    strcpy(res, mx);
+    return res;
+}
+
+char *getIntLitName(int c) {
+    char mx[MAX_LIT];
+    sprintf(mx, "int@%d", c);
+    char *res = malloc(sizeof(char) * (strlen(mx) + 1));
+    strcpy(res, mx);
+    return res;
+}
+
+char *generateNewlbl() {
+    static int cnt = 0;
+    char mx[MAX_VAR];
+    sprintf(mx, "$help_label_%d$", cnt);
+    char *res = malloc(sizeof(char) * (strlen(mx) + 1));
+    strcpy(res,mx);
+    cnt++;
+    return res;
+}
+
+void moveVariableToGlobal(char *globalVar, char *localVar) {
+    move(globalVar, localVar);
+}
+void handleGlobalVariable(char *globalVar, char *localVar) {
+    Symbol *Cur;
+
+    // Check if the variable exists in the global table
+    if (getFromGlobaltbl(globalVar, &Cur)) {
+        moveVariableToGlobal(globalVar, localVar);
+    } else {
+        // If not, define the variable and handle recursion
+        char *defSkipLabel = generateLabel();
+        printf("JUMPIFEQ %s GF@___$recursion$___ bool@true\n", defSkipLabel);
+        defineVariable(globalVar);
+        printf("LABEL %s\n", defSkipLabel);
+        free(defSkipLabel);
+
+        moveVariableToGlobal(globalVar, localVar);
+
+        // Insert the variable into the global symbol table
+        Symbol defined;
+        SymtblInit(&defined);
+        symtb_insert(runInfo->globalFrame, globalVar, defined);
+        RemoveSymbol(runInfo->currentLVL->currentTab,&defined);
+    }
+}
+void passVariablesToGlobal() {
+    int localLevel = runInfo->currentLVL;
+    for (int i = 0; i < sizeof(runInfo->currentLVL); i++) {
+        char globalVar[MAX_VAR];
+        sprintf(globalVar, llvlVarForm, "GF", runInfo->currentLVL->currentTab, localLevel);
+        char *localVar = getVarName(runInfo->currentLVL->currentTab);
+=======
 }
 
 void generate_binary_arithmetic_code(node_t* node) {
@@ -123,6 +359,7 @@ void generate_binary_arithmetic_code(node_t* node) {
             break;
     }
 }
+>>>>>>> d27f9842d0298b81609793bc523c57c6732cd08e
 
 // Helper function to get a temporary variable name
 char* get_temp_variable() {
@@ -247,6 +484,26 @@ void generate_statement_list_code(node_t* node) {
 }
 
 
+<<<<<<< HEAD
+void moveGlobalToVariable(char *variable, char *globalVar) {
+    move(variable, globalVar);
+}
+
+void processGlobalVariable(char *variable, char *globalVar) {
+    Symbol *Cur;
+
+    // Check if the variable exists in the global tbl
+    if (getFromGlobaltbl(globalVar, &Cur)) {
+        moveGlobalToVariable(variable, globalVar);
+    }
+}
+void returnPassedVariables() {
+    int localLevel = runInfo->currentLVL;
+    for (int i = 0; i < sizeof(runInfo->currentLVL); i++) {
+        char globalVar[MAX_VAR];
+        sprintf(globalVar, llvlVarForm, "GF", runInfo->currentLVL->currentTab, localLevel);
+        char *variable = getVarName(runInfo->currentLVL->currentTab);
+=======
 void generate_statement_code(node_t* node) {
     switch (node->current->type) {
         case T_LET:
@@ -274,6 +531,7 @@ void generate_statement_code(node_t* node) {
             // Handle other statement types if necessary
             break;
     }
+>>>>>>> d27f9842d0298b81609793bc523c57c6732cd08e
 
     if (node->left != NULL) {
         generate_expression_code(node->left);
@@ -281,9 +539,48 @@ void generate_statement_code(node_t* node) {
 }
 
 
+<<<<<<< HEAD
+void writeCallPutprmtr(Token prmtr, Symbol CFunc) {
+    printf("PUSHS %s\n", globVar1);
+    printf("ADD %s %s int@1\n", globVar1, globVar1);
+    if (prmtr.Category == TC_ID) {
+        char *varN = getVarName(prmtr.value.stringVal);
+        printf("PUSHS %s\n", varN);
+        free(varN);
+    } else {
+        char *litN = getLitName(prmtr);
+        printf("PUSHS %s\n", litN);
+        free(litN);
+    }
+    printf("PUSHS %s\n", globVar1);
+}
+
+void functionCallPutprmtr(Token prmtr, Symbol CFunc) {
+    if (strcmp(CFunc.id, "write") == 0) {
+        writeCallPutprmtr(prmtr, CFunc);
+        return;
+    }
+
+    if (prmtr.Category == TC_ID) {
+        char *varN = getVarName(prmtr.value.stringVal);
+        printf("PUSHS %s\n", varN);
+        free(varN);
+    } else {
+        char *litN = getLitName(prmtr);
+        printf("PUSHS %s\n", litN);
+        free(litN);
+    }
+}
+
+
+void generateInt2Double() {
+    printf(
+        "\n LABEL $int2Double" \
+=======
 void generateInt2Double() {
     printf(
         "\n LABEL $Int2Double" \
+>>>>>>> d27f9842d0298b81609793bc523c57c6732cd08e
         "\n PUSHFRAME" \
         "\n DEFVAR LF@%retval" \
         "\n INT2FLOAT LF@%retval LF@%0" \
@@ -352,7 +649,11 @@ void generateLength()
 
 void generateSubstring() {
     printf(
+<<<<<<< HEAD
+        "\n LABEL $substr" \
+=======
         "\n LABEL $substring" \
+>>>>>>> d27f9842d0298b81609793bc523c57c6732cd08e
         "\n PUSHFRAME" \
         "\n DEFVAR LF@%retval" \
         "\n MOVE LF@%retval string@" \
